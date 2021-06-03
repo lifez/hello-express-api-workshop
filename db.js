536f3e55188401aef46543c921b2fc30289bc97b
Phawin <lifez999@gmail.com>
2021-06-03 15:43:49 +0700

    Add get, post, patch, delete

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-var sqlite3 = require('better-sqlite3').verbose();
-var db = new sqlite3.Database("./db.sqlite")
-db.run("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, description TEXT)");
+const Database = require('better-sqlite3')
+const db = new Database("./db.sqlite")
+db.exec("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, description TEXT)");
 
 
 module.exports = db
--- index.js
+++ index.js
@@ -10,6 +10,34 @@ app.post('/', (req, res) => {
   res.send(req.body)
 })
 
+app.post('/new', (req, res) => {
+  const { title, description } = req.body
+  const statement = db.prepare(`INSERT INTO books (title, description) VALUES (?, ?)`)
+  const info = statement.run(title, description)
+  res.json(info)
+})
+
+app.get('/', (_, res) => {
+  const statement = db.prepare('SELECT * from books')
+  const books = statement.all()
+  res.json(books)
+})
+
+app.patch('/:bookId', (req, res) => {
+  const { bookId } = req.params
+  const { title } = req.body
+  const statement = db.prepare('UPDATE books SET title = ? WHERE id = ?')
+  const info = statement.run(title, bookId)
+  res.json(info)
+})
+
+app.delete('/:bookId', (req, res) => {
+  const { bookId } = req.params
+  const statement = db.prepare('DELETE from books WHERE id = ?')
+  const info = statement.run(bookId)
+  res.json(info)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
